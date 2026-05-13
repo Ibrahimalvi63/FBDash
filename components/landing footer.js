7d0c6f196250ca24c048d6aa7a2987f44e733467
Ibrahim <ibrahimalvi63@gmail.com>
2026-05-13 11:47:32 +0600

    initial commit

---
+++ components/landing footer.js
@@ -0,0 +1,21 @@
+export default function LandingFooter() {
+    return (
+        <div>
+            <div className="w-full text-gray-400 px-3 py-5 bg-blue-900 flex items-start justify-between">
+                <div className="">
+                    <h1 className="text-xl font-bold">FBDash</h1>
+                    <h2 className="text-sm">Smart solution for you</h2>
+                </div>
+                <div className="">
+                    <h3 className="text-xl font-bold">Any problem</h3>
+                    <p className="text-sm"><b>Phone: </b> 01944-223552</p>
+                    <p className="text-sm"><b>Email: </b> ibrahimalvi@gmail.com</p>
+                </div>
+            </div>
+
+            <div className="bg-blue-950 text-center text-gray-400">
+                <p className="py-3 text-sm">© 2026 FBDash — Smart Facebook Business Management.</p>
+            </div>
+        </div>
+    )
+}
---
+++ components/landing nav.js
@@ -0,0 +1,8 @@
+export default function LandingNav() {
+    return (
+        <div className="w-full px-5 py-2 bg-blue-500">
+            <h1 className="text-2xl text-gray-200 font-bold">FBDash</h1>
+            <h2 className="text-gray-200">Smart solution for your facebook bussiness</h2>
+        </div>
+    )
+}
